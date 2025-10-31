e41a9097269b68069bdd95acbc9af9b443c63dc9
haris49 <hariis.459@gmail.com>
2025-10-31 09:36:56 +0700

    Menambahkan input field Nama Lengkap dengan pengaturan tata letak vertikal

--- app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
@@ -1,12 +1,18 @@
 package com.example.questuserinput_015
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -26,5 +32,20 @@ fun FormDataDiri(modifier: Modifier
     var gender: List<String> = listOf("Laki-Laki","Perempuan")
 
     Column (modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
 
+        horizontalAlignment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(350.dp),
+
+            label = { Text(text = "Nama Lengkap") },
+
+            onValueChange =  {
+                textNama = it
+            }
+         )
+   }
 }
