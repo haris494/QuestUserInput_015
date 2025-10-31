3b7ef48e3985165156829bed482de1ab7c0b8c53
haris49 <hariis.459@gmail.com>
2025-10-31 09:27:11 +0700

    Menambahkan komponen Column dengan padding atas 50dp

--- app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
@@ -1,11 +1,14 @@
 package com.example.questuserinput_015
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -21,4 +24,7 @@ fun FormDataDiri(modifier: Modifier
     var jenis by remember { mutableStateOf("") }
 
     var gender: List<String> = listOf("Laki-Laki","Perempuan")
+
+    Column (modifier = Modifier.padding(top = 50.dp),
+
 }
