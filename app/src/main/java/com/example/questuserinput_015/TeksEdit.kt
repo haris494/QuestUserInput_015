6a81cf64c4fc5a1e74498649231da97ec13833c3
haris49 <hariis.459@gmail.com>
2025-10-31 09:20:20 +0700

    Menambahkan state variabel untuk menyimpan input alamat pengguna

--- app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
@@ -17,5 +17,5 @@ fun FormDataDiri(modifier: Modifier
 
     //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
     var nama by remember { mutableStateOf("") }
-
+    var alamat by remember { mutableStateOf("") }
 }
