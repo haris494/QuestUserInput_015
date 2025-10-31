1314fe1da9e57f62a2d5eff6c63ebdd771008743
haris49 <hariis.459@gmail.com>
2025-10-31 09:55:29 +0700

    Menambahkan RadioButton dan teks untuk memilih jenis kelamin pengguna

--- app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -50,8 +51,20 @@ fun FormDataDiri(modifier: Modifier
             }
          )
 
-        Row {
+    Row {
         gender.forEach { item ->
             Row(modifier = Modifier.selectable(
                 selected = textJK == item,
+
+                onClick = { textJK = item}
+            ), verticalAlignment = Alignment.CenterVertically){
+                RadioButton(
+                    selected = textJK == item,
+
+                    onClick = {
+                        textJK = item
+                    })
+                Text(text = item)
             }
+        }
+    }   
