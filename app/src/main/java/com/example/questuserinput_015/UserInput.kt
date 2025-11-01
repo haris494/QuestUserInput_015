318324e201af12c0a9cef8261fcba0172d35ef4b
haris49 <hariis.459@gmail.com>
2025-11-01 20:19:08 +0700

    Buat composable UserInput yang nyimpan data input nama sementara.

---
+++ app/src/main/java/com/example/questuserinput_015/UserInput.kt
@@ -0,0 +1,11 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun UserInput(modifier: Modifier = Modifier) {
+    var nama by remember { mutableStateOf("") }
+}
