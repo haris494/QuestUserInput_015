cc4b0b03a67c71d06c6440ce3e10d872f9dbb9e6
haris49 <hariis.459@gmail.com>
2025-11-01 20:35:01 +0700

    Menampilkan pilihan jenis kelamin dalam bentuk radio button, di mana pengguna bisa memilih satu opsi dari daftar listGender.

--- app/src/main/java/com/example/questuserinput_015/UserInput.kt
+++ app/src/main/java/com/example/questuserinput_015/UserInput.kt
@@ -1,11 +1,13 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
@@ -70,6 +72,17 @@ fun UserInput(modifier: Modifier = Modifier) {
                     singleLine = true,
                     modifier = Modifier.fillMaxWidth()
                 )
+                Column {
+                    Text("Jenis Kelamin", fontWeight = FontWeight.SemiBold)
+                    listGender.forEach { gender ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .selectable(
+                                    selected = (jenisKelamin == gender),
+                                    onClick = { jenisKelamin = gender }
+                                )
+
             }
 
             }
