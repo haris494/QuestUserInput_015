be133b355dd71d3836e84527b087b62b4c21e7c7
haris49 <hariis.459@gmail.com>
2025-11-01 20:27:01 +0700

    Membuat layout vertikal (Column) yang mengisi seluruh layar, memiliki latar belakang abu-abu muda, diberi padding 16dp, dan isi komponennya di tengah secara horizontal.

--- app/src/main/java/com/example/questuserinput_015/UserInput.kt
+++ app/src/main/java/com/example/questuserinput_015/UserInput.kt
@@ -1,9 +1,16 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun UserInput(modifier: Modifier = Modifier) {
@@ -14,4 +21,12 @@ fun UserInput(modifier: Modifier = Modifier) {
 
     val listGender = listOf("Laki-laki", "Perempuan")
     val listStatus = listOf("Lajang", "Menikah", "Duda/Janda")
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFF5F5F5))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 }
